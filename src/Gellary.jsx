b8c89e236c84dc72625dcfb417a911520fdba38b
my1335667-art <my1335667@gmail.com>
2026-04-10 19:44:08 +0200

    Update Gellary.jsx

--- src/Gellary.jsx
+++ src/Gellary.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect, useRef } from "react";
 import "./Gellary.css";
 
 const firstThreeVideos=[
-  { id: 1, src: "/videos/video1.mp4", title: "Cinematic Travel" },
+  { id: 1, src: "/videos/IMG_5063.MOV", title: "Cinematic Travel" },
   { id: 2, src: "/images/testV2.mp4", title: "Brand Story" },
   { id: 3, src: "/images/testV3.mp4", title: "Commercial Shot" },
 
   
-  { id: 1, src: "/videos/video1.mp4", title: "Cinematic Travel" },
+  { id: 1, src: "/videos/IMG_5063.MOV", title: "Cinematic Travel" },
   { id: 2, src: "/images/testV2.mp4", title: "Brand Story" },
   { id: 3, src: "/images/testV3.mp4", title: "Commercial Shot" },
 ]
